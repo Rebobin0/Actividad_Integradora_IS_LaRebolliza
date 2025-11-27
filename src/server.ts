aa7704eb7507546c226ecdd372da2a5f8098e880
Emiliano Rebolledo <rebolledonavarreteemiliano@gmail.com>
2025-11-27 12:18:14 -0600

    Commit hasta el video 16 de la parte 1

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
 import express from 'express'
+import router from './router'
+import db from './config/db'
 
+// Conexion a BD
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('Conexion a la BD exitosa')
+    } catch (error) {
+        console.log(error)
+        console.log('Hubo un error al conectar a la BD')
+    }
+}
+
+connectDB()
 const server = express()
-
-//Routing
-server.get('/', (req, res) => {
-    const datos = [
-        { id: 1, nombre: 'Emiliano' },
-        { id: 2, nombre: 'Gaspar' },
-    ]
-
-    res.send(datos)
-})
+server.use('/api/products', router)
 
 export default server
